d509413936f172b980be5fbbabcff16dac4bbd1a
PaulHempel <paul.hempel@kabelmail.de>
2022-04-11 06:01:37 +0200

    change setup.py requierements to direct list for usage with pipenv-setup Add Pipfile and Pipfile.lock for usage in pipenv

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = []
-
 setup(
     name="observables",
     version="0.0.1",
@@ -23,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MetFred/python.observables",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3.9",
     ],
